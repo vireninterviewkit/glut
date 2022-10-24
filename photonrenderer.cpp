26162c3da7419ae34a2c835932c53c6a95811733
Viren <vireninterviewkit@gmail.com>
2022-10-24 00:49:27 -0700

    more

--- photonrenderer.cpp
+++ photonrenderer.cpp
@@ -5,3 +5,29 @@ PhotonRenderer::PhotonRenderer(QWidget *parent)
 {
 
 }
+
+QSize PhotonRenderer::sizeHint() const {
+    return QSize(400, 300);
+}
+
+void PhotonRenderer::initializeGL() {
+    glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    glEnable(GL_POINT_SMOOTH);
+}
+
+void PhotonRenderer::paintGL() {
+    //qglClearColor(QColor::fromRgb(255, 255, 255));
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+//    draw_points();
+//    draw_segments();
+//    draw_vertices();
+//    draw_edges();
+}
+
+void PhotonRenderer::resizeGL(int width, int height) {
+    int side = qMin(width, height);
+    glViewport((width - side) / 2, (height - side) / 2, side, side);
+}
--- photonrenderer.h
+++ photonrenderer.h
@@ -1,14 +1,45 @@
 #ifndef PHOTONRENDERER_H
 #define PHOTONRENDERER_H
 
+
+#include <iostream>
+#include <vector>
+#include <boost/polygon/polygon.hpp>
+#include <boost/polygon/voronoi.hpp>
 #include <QOpenGLWidget>
 #include <QWidget>
+using namespace boost::polygon;
 
 class PhotonRenderer : public QOpenGLWidget
 {
     Q_OBJECT
 public:
     explicit PhotonRenderer(QWidget *parent = nullptr);
+
+    QSize sizeHint() const;
+protected:
+    void initializeGL();
+    void paintGL();
+    void resizeGL(int width, int height);
+private:
+    typedef double coordinate_type;
+    typedef point_data<coordinate_type> point_type;
+    typedef segment_data<coordinate_type> segment_type;
+    typedef rectangle_data<coordinate_type> rect_type;
+    typedef voronoi_builder<int> VB;
+    typedef voronoi_diagram<coordinate_type> VD;
+    typedef VD::cell_type cell_type;
+    typedef VD::cell_type::source_index_type source_index_type;
+    typedef VD::cell_type::source_category_type source_category_type;
+    typedef VD::edge_type edge_type;
+    typedef VD::cell_container_type cell_container_type;
+    typedef VD::cell_container_type vertex_container_type;
+    typedef VD::edge_container_type edge_container_type;
+    typedef VD::const_cell_iterator const_cell_iterator;
+    typedef VD::const_vertex_iterator const_vertex_iterator;
+    typedef VD::const_edge_iterator const_edge_iterator;
+
+    static const std::size_t EXTERNAL_COLOR = 1;
 
 signals:
 
