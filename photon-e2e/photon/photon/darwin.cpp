37efa4421356aff51628af0a60338390a30a5673
Viren <vireninterviewkit@gmail.com>
2022-10-26 22:01:12 -0700

    cb

--- photon-e2e/photon/photon/darwin.cpp
+++ photon-e2e/photon/photon/darwin.cpp
@@ -6,12 +6,16 @@
 //
 
 #ifdef __APPLE_CC__
+#define GL_SILENCE_DEPRECATION
+#include <OpenGL/gl.h>
 #include <GLUT/glut.h>
 #else
+#include <GL/gl.h>
 #include <GL/glut.h>
 #endif
 #include <iostream>
 #include <assert.h>
+
 #include "darwin.hpp"
 
 static darwin *g_darwin_ = 0;
@@ -35,11 +39,10 @@ void darwin::construct(int argc,  char * argv[]) {
   glutInitWindowPosition(80, 80);
   glutInitWindowSize(400, 300);
   glutCreateWindow("Photon Darwin Window");
-  g_darwin_ = this;
+  g_darwin_ = this; //TODO: ugly work around
   glutDisplayFunc(update_ui);
   glutMainLoop();
 }
-
 
 void darwin::show_scene(unsigned int flags) {
   std::cout << __PRETTY_FUNCTION__ << std::endl;
